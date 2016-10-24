d20508accf1961b96017ad5e712d4a3a8893e9b0
andrewpatterson3001 <andrew.patterson.3001@gmail.com>
2016-10-24 14:33:00 -0700

    Extract title to its own file, setup webpack and babel

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,25 +1,18 @@
-/* global React ReactDOM */
+var React = require('react')
+var ReactDOM = require('react-dom')
 
 var div = React.DOM.div
 var h1 = React.DOM.h1
 
-var MyTitle = React.createClass({
-  render () {
-    return (
-      div(null,
-        h1(null, 'Check out this component!')
-      )
-    )
-  }
-})
+var MyTitle = require('./MyTitle')
 
 var MyTitleFact = React.createFactory(MyTitle)
 
 var MyFirstComponent = (
   div(null,
-    MyTitleFact(null),
-    MyTitleFact(null),
-    MyTitleFact(null)
+    MyTitleFact({title: 'Props are great!', color: 'rebeccapurple'}),
+    MyTitleFact({title: 'Use props everywhere!', color: 'mediumaquamarine'}),
+    MyTitleFact({title: 'Props are the best!', color: 'peru'})
   )
 )
 
