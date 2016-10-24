710245de3cc5928f23872f5ae772caad4c0e4449
andrewpatterson3001 <andrew.patterson.3001@gmail.com>
2016-10-24 16:16:49 -0700

    add Layout and nested Routes

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -2,13 +2,16 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const Landing = require('./Landing')
 const ChatPage = require('./ChatPage')
+const Layout = require('./Layout')
 const ReactRouter = require('react-router')
-const { Router, Route, hashHistory } = ReactRouter
+const { Router, Route, hashHistory, IndexRoute } = ReactRouter
 
 const App = () => (
   <Router history={hashHistory}>
-    <Route path='/' component={Landing} />
-    <Route path='/chat' component={ChatPage} />
+    <Route path='/' component={Layout}>
+      <IndexRoute component={Landing} />
+      <Route path='/chat' component={ChatPage} />
+    </Route>
   </Router>
 )
 
