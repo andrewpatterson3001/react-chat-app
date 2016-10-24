59e9107ab37e3f9ac5dab025703f4b57fa813b6d
andrewpatterson3001 <andrew.patterson.3001@gmail.com>
2016-10-23 23:50:11 -0700

    writing stateless React components using React.createClass, React.createElement, and ReactDOM.render

---
+++ js/ClientApp.js
@@ -0,0 +1,26 @@
+/* global React ReactDOM */
+
+var div = React.DOM.div
+var h1 = React.DOM.h1
+
+var MyTitle = React.createClass({
+  render () {
+    return (
+      div(null,
+        h1(null, 'Check out this component!')
+      )
+    )
+  }
+})
+
+var MyTitleFact = React.createFactory(MyTitle)
+
+var MyFirstComponent = (
+  div(null,
+    MyTitleFact(null),
+    MyTitleFact(null),
+    MyTitleFact(null)
+  )
+)
+
+ReactDOM.render(MyFirstComponent, document.getElementById('app'))
