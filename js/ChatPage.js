8b7b55079dbbe7b3d95fd520f9f1c0ebe624ba16
andrewpatterson3001 <andrew.patterson.3001@gmail.com>
2016-10-24 18:16:14 -0700

    add messages to page onSubmit

--- js/ChatPage.js
+++ js/ChatPage.js
@@ -8,14 +8,22 @@ class ChatPage extends React.Component {
 
 		this.state = {
 			loggedUser: (this.props.params.user || ''),
-			typedMessage: ''
+			typedMessage: '',
+			messages: this.props.route.messages
 		}
 
 		this.handleMessageChange = this.handleMessageChange.bind(this)
+		this.addMessage = this.addMessage.bind(this)
 	}
 
 	handleMessageChange (event) {
 		this.setState({ typedMessage: event.target.value })
+	}
+
+	addMessage (event) {
+		this.setState((prevState, props) => {
+			return {messages: prevState.messages.concat({"messageContent":this.state.typedMessage, "user":this.state.loggedUser}), typedMessage: ''}
+		})
 	}
 
 	render () {
@@ -25,18 +33,20 @@ class ChatPage extends React.Component {
   				<h1>Chat Here!!</h1>
   				<h3>Logged in as {this.state.loggedUser}.</h3>
     			<div className='messages'>
-      				{this.props.route.messages.map((message, index) => (
+      				{this.state.messages.map((message, index) => (
         				<MessageLine {...message} key={index} />
       				))}
     			</div>
+    			<form onSubmit={this.addMessage}>
     			  <input className='search' type='text' placeholder='Your Message Here' value={this.state.typedMessage} onChange={this.handleMessageChange} />
+    			</form>
   			</div>
 			)
 	}
 }
 
-module.exports = ChatPage
-
 ChatPage.propTypes = {
   messages: React.PropTypes.arrayOf(React.PropTypes.object)
 }
+
+module.exports = ChatPage
